f2d72f43bd81fb5e4294142a86aebdf516470d02
diyor <diyor7775@gmail.com>
2026-02-05 16:26:55 +0500

    js

---
+++ script.js
@@ -0,0 +1,16 @@
+ var name =prompt("Enter name").trim();
+ var age =prompt("Enter age").trim();
+
+ console.log(name, age)
+
+var gander = prompt ("enter gander").trim();
+  
+
+if (gander == "man"){
+  var gander = prompt ("armiyaga borganmisiz")
+  console.log("borishingiz kerak")
+} else {
+    console.log("raxmat")
+}
+
+  
