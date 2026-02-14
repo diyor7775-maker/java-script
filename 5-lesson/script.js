09ba903d5d3441727c39bf7524f7102f86a228d8
diyor <diyor7775@gmail.com>
2026-02-14 16:02:58 +0500

    js

---
+++ 5-lesson/script.js
@@ -0,0 +1,15 @@
+ var text =prompt("Matn kiriting" );
+ console.log(text);
+
+ var alphabet =prompt("alifbodagi harflardan birini kiriting" );
+ console.log(alphabet );
+  
+
+ for (var i = 0; i <= text.length; i++) {
+        console.log(i);
+        if (alphabet== alphabet.toLowerCase()){
+     console.log(alphabet.toLowerCase());
+     
+}
+}
+
