3157144b7a69a16e9b108be092a39d01498cf653
diyor <diyor7775@gmail.com>
2026-02-20 22:25:34 +0500

    js

---
+++ 6-lesson/script.js
@@ -0,0 +1,29 @@
+var a = prompt(" Sonni kiriting");
+
+
+   var month = "A"
+    if (month == "A","1"){
+   console.log( "yanvar");
+   }
+
+
+    var month = "B"
+    if (month == "B","2"){
+  console.log("fevral");
+  
+   }
+    var month = "C"
+    if (month == "C","3"){
+  console.log("mart");
+  
+   }
+    var month = "D"
+    if (month == "D","4"){
+  console.log("aprel");
+  
+   }
+    var month = "E"
+    if (month == "E","5"){
+  console.log("may");
+  
+   }
---
+++ konkulyator 2/script.js
@@ -0,0 +1,11 @@
+var a = prompt("suz  kiriting:");
+
+var arr = ["salom", "salom", "salom", "salom"];
+
+console.log(4);
+
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+console.log(arr[3]);
+
