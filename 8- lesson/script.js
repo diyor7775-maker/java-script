8f8da74ab8ed3fda0c8986a6633d485a79f00c9c
diyor <diyor7775@gmail.com>
2026-02-26 09:33:16 +0500

    js

---
+++ 8- lesson/script.js
@@ -0,0 +1,32 @@
+//var input = prompt("Telefon yoki email kiriting:");
+
+//var numbers = input.replace("+", "");
+
+//if (numbers.slice(0, 3) == "998" && numbers.length >= 10) {
+
+  //var rest = numbers.slice(3);
+
+ // console.log(
+   // "+998 " +
+  //  rest.slice(0, 3) + " " +
+  //  rest.slice(3, 5) + " " +
+   // rest.slice(5, 7)
+//  );
+
+//} else {
+
+//  console.log(input + "@gmail.com");
+
+//}
+
+var kod = prompt("Kod kiriting (90, 93, 97 ...):");
+
+if (kod == "90") {
+    console.log("+998 90 565 35 53 (BEELINE)");
+}
+else if (kod == "93") {
+    console.log("+998 93 565 35 53 (UCELL)");
+}
+else if (kod == "97") {
+    console.log("+998 97 565 35 53 (MOBIUZ)");
+}
