3fd75e3fcf6434bcc263b307a47806de769819c3
diyor <diyor7775@gmail.com>
2026-03-03 11:36:36 +0500

    js

---
+++ xonalar/script.js
@@ -0,0 +1,21 @@
+
+var javob = prompt("Eshik bormi? (ha/yo'q)");
+
+if (javob == "ha") {
+  var eni = prompt("Eni:");
+  var boyi = prompt("Bo'yi:");
+  alert("yuzi: " + (eni * boyi));
+} else {
+  alert("Eshik yo'q");
+}
+
+var savol =prompt ("Deraza bormi? (ha/yo'q)");
+
+if (savol == "ha"){
+  var eni =prompt ("Eni:");
+  var eni =prompt ("Bo'yi:");
+  alert ("yuzi:" + (eni * boyi));
+}else{
+  alert("Deraza yo'q");
+}
+
